d0642649b26d0cab145188acc9fa0e28250744c4
Nikil V. Shyamsunder <nvs26@cornell.edu>
2025-11-16 12:23:53 -0500

    fix relative imports

--- allo/backend/xls/example.py
+++ allo/backend/xls/example.py
@@ -2,7 +2,7 @@ import allo
 from allo.ir.types import int32
 from allo._mlir.dialects import func as func_d
 
-from .xls import MlirToDslxLowerer, debug_print_ir
+from . import MlirToDslxLowerer, debug_print_ir
 
 
 def allo_gemm(A: int32[32, 32], B: int32[32, 32]) -> int32[32, 32]:
